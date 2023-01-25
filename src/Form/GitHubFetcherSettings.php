9a92f5f3159d715337d3e5f8ccd0dd29e4f08e89
unknown <tasabina@yandex.ru>
2023-01-25 20:29:53 +1300

    Update UI Config page

--- src/Form/GitHubFetcherSettings.php
+++ src/Form/GitHubFetcherSettings.php
@@ -4,9 +4,13 @@ namespace Drupal\github_fetcher\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 
 class GitHubFetcherSettings extends FormBase
 {
+    protected const GITHUB_API_VALUES = 'github_fetcher:values';
+
     public function getFormId(): string
     {
         return 'github_fetcher_form';
@@ -14,6 +18,11 @@ class GitHubFetcherSettings extends FormBase
 
     public function buildForm(array $form, FormStateInterface $form_state): array
     {
+        $values = \Drupal::state()->get(self::GITHUB_API_VALUES) ??
+                    [
+                        'repo_url' => '',
+                        'github_token' => '',
+                    ];
         $form['github_details'] = [
             '#type' => 'details',
             '#title' => $this->t('GitHub url & access token'),
@@ -25,6 +34,7 @@ class GitHubFetcherSettings extends FormBase
             '#group' => 'github_details',
             '#description' => $this->t('Description'),
             '#required' => TRUE,
+            '#default_value' => $values['repo_url'],
         ];
 
         $form['github_token'] = [
@@ -33,14 +43,23 @@ class GitHubFetcherSettings extends FormBase
             '#group' => 'github_details',
             '#description' => $this->t('Description'),
             '#required' => TRUE,
+            '#default_value' => $values['github_token'],
         ];
               
-          $form['submit'] = [
-            '#type' => 'submit',
-            '#value' => $this->t('Submit'),
+          $form['actions'] = [
+            '#type' => 'actions',
+            'submit' => [
+                '#type' => 'submit',
+                '#value' => $this->t('Save'),
+                '#button_type' => 'primary',
+            ],
+            'cancel' => [
+                '#type' => 'button',
+                '#value' => $this->t('Cancel'),
+            ],
         ];
       
-          return $form;
+        return $form;
     }
 
     public function validateForm(array &$form, FormStateInterface $form_state)
@@ -55,6 +74,9 @@ class GitHubFetcherSettings extends FormBase
 
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
+        $submitted_value = $form_state->cleanValues()->getValues();
+        \Drupal::state()->set(self::GITHUB_API_VALUES, $submitted_value);
+
         $this->messenger()->addStatus(
             $this->t('GitHub account URL is @url', ['@url' => $form_state->getValue('repo_url')])
         );
